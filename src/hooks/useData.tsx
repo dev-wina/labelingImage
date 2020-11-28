77f6fa15db11313e108a7028d5c741ee6379e9eb
dev-wina <debugingsy@gmail.com>
2020-11-28 12:19:41 +0900

    Add useImage hooks
    Refactor hooks

---
+++ src/hooks/useData.tsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "~modules"
+import { useCallback } from "react"
+import { initData, modifyData, Video } from "~modules/data";
+
+export default function useData() {
+    const data = useSelector((state: RootState) => state.data)
+    const dispatch = useDispatch()
+    const init = useCallback((payload: Video[]) => dispatch(initData(payload)), [dispatch])
+    const modify = useCallback((payload: Video) => dispatch(modifyData(payload)), [dispatch])
+    const findById = ( id : number ) => {
+        return data.find( (item:Video) => (item.id === id) ) || null
+    }
+    return {
+        data,
+        init,
+        findById,
+        modify
+    }
+}
+
+ 
---
+++ src/hooks/useImage.tsx
@@ -0,0 +1,30 @@
+import { MutableRefObject, useEffect } from 'react'
+
+export default function useImage(ref: MutableRefObject<any>, image: any){
+    useEffect(()=>{
+        const handleKeyDown = (e) => {
+            alert(`확인확인 ${e.key}`)
+            if(e.key === "Space"){
+                useImage(ref, image)        
+            }
+        }
+
+        const canvas: HTMLCanvasElement = ref.current
+
+        const img = new Image()
+        img.src = image
+        //화면에 정확히 적용되지 않음
+        img.onload = () => canvas.getContext('2d')?.drawImage(img, 0, 0, img.width*(canvas.height/img.height), img.height*(canvas.height/img.height))
+
+
+        if (ref && ref.current) {
+            ref.current.focus();
+            ref.current.addEventListener("keydown", handleKeyDown )
+        }
+
+        return () => {
+            ref.current.removeEventListener("keydown", handleKeyDown )
+        }
+        
+    },[image])
+}
