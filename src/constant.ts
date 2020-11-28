09613a365cda68df4cd57264bea05fcf21358858
dev-wina <debugingsy@gmail.com>
2020-11-28 11:19:09 +0900

    Repactor Annotator Home page

---
+++ src/constant.ts
@@ -0,0 +1,11 @@
+export enum PAGE_PATHS {
+    HOME = "/annotator_home",
+    DATASET_LABEL = "/dataset_label"
+}
+
+export enum PAINT_RECT_MODE {
+    NONE = "none",
+    CREATE = "createRect",
+    MOVE = "moveRect",
+    RESIZE = "resizeRect"
+}
